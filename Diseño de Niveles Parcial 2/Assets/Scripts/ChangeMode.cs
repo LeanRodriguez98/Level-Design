859c3808f0a4191374a8a20307a3592c54615e9e
Unknown <lean1998@hotmail.com>
2018-07-06 04:21:46 -0300

    Change Mode

---
+++ Diseño de Niveles Parcial 2/Assets/Scripts/ChangeMode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMode : MonoBehaviour {
+    public GameObject Probilder;
+    public GameObject Level;
+    private bool switchState;
+	// Use this for initialization
+	void Start () {
+        Probilder.SetActive(false);
+        Level.SetActive(true);
+        switchState = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C) && !switchState)
+        {
+            Probilder.SetActive(true);
+            Level.SetActive(false);
+            switchState = true;
+        }else 
+        if (Input.GetKeyDown(KeyCode.C) && switchState)
+        {
+            Probilder.SetActive(false);
+            Level.SetActive(true);
+            switchState = false;
+        }
+    }
+}
